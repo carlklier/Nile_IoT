85189f1493bc38d7c2380ed4f362c7fe04f1f521
dnteague <dnteague@ncsu.edu>
2020-02-11 21:52:51 -0500

    modifying project structure

--- loadtest_webservice/config.py
+++ loadtest_webservice/config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://daltonteague@localhost/loadtest_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://<username>@localhost/loadtest_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
