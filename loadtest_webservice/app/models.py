a5e5a9707fb113023709bf5bdfef2d9a4921c05a
Eric <ewmcalli@ncsu.edu>
2020-02-12 12:05:52 -0500

    Added models from ER diagram

--- loadtest_webservice/app/models.py
+++ loadtest_webservice/app/models.py
@@ -23,3 +23,53 @@ class Test(db.Model):
             'end':self.end,
             'workers':self.workers
         }
+
+class Request(db.Model):
+    __tablename__ = 'requests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time_sent = db.Column(db.TIMESTAMP)
+    request_type = db.Column(db.String())
+    request_length = db.Column(db.Integer())
+    response_type = db.Column(db.String())
+    response_length = db.Column(db.Integer())
+    duration = db.Column(db.TIMESTAMP)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'time_sent': self.time_sent,
+            'request_type': self.request_type,
+            'request_length': self.request_length,
+            'response_type': self.response_type,
+            'response_length': self.response_length,
+            'duration': self.duration
+        }
+
+class SystemMetric(db.Model):
+    __tablename__ = 'system metrics'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.TIMESTAMP)
+    metric_type = db.Column(db.String())
+    metric_value = db.Column(db.BigInteger)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'time': self.time,
+            'metric type': self.metric_type,
+            'metric value': self.metric_value
+        }
