b405f832f71454901d8f56250e8a43caca9526bb
Eric <ewmcalli@ncsu.edu>
2020-02-12 11:17:37 -0500

    Pipenv used over virtualenv

--- loadtest_webservice/config.py
+++ loadtest_webservice/config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://<username>@localhost/loadtest_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://senior_user:password@localhost/loadtestdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
